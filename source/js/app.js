85b54aeff117a17b3dd5180b9f6fc797d19c21bb
Alexander <dev@salex.pro>
2017-10-30 03:46:46 +0200

    responsive

--- source/js/app.js
+++ source/js/app.js
@@ -4,6 +4,46 @@
 
 // $(document).foundation();
 
+
+var $el = $(".slides");
+var elWidth = $el.outerWidth();
+var elHeight = $el.outerHeight();
+
+var $wrapper = $('.presentation');
+
+$(window).resize(function(){
+    var sizes = { 
+      size: {
+        width: $wrapper.width(),
+        height: $wrapper.height()
+      }
+    }
+    doResize(null, sizes)
+})
+
+function doResize(event, ui) {
+  
+  var scale, origin;
+    
+  scale = Math.min(
+    ui.size.width / elWidth,
+    ui.size.height / elHeight
+  );
+  
+  $el.css({
+        transform: "translate3d(-50%, -50%, 0) scale(" + scale + ")"
+  });
+  
+}
+
+var starterData = { 
+  size: {
+    width: $wrapper.width(),
+    height: $wrapper.height()
+  }
+}
+doResize(null, starterData);
+
 var current = 1;
 
 $('.switcher_button').click(function(){
@@ -16,8 +56,13 @@ $('.switcher_button').click(function(){
     };
     if(current > 1){
         $('.switcher_button--prev').prop('disabled', false);
-    } else {
+    } else{
         $('.switcher_button--prev').prop('disabled', true);
+    }
+    if(current < 46){
+        $('.switcher_button--next').prop('disabled', false);
+    } else {
+        $('.switcher_button--next').prop('disabled', true);
     }
 })
 
